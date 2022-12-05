459af19b2a677be67695b280ce854f526072c56a
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-12-05 14:33:45 +0000

    Add drop downs for all pages

--- products.js
+++ products.js
@@ -140,11 +140,45 @@ if (today.getHours() >= 9 && today.getHours() < 17) {
 
 
 const burger = document.querySelector(".burger")
+const navHomeLink = document.querySelector(".homeLink")
 const dropDown = document.querySelector(".dropDown")
+const heroSection = document.querySelector(".hero-section-products")
+const secondContactButton = document.querySelector(".contactNew")
+const dropDownUl = document.querySelectorAll(".dropDown ul li a")
+
 
 burger.addEventListener("click", (e) => {
     dropDown.classList.toggle("dropDownAdd")
 })
+
+const options = { }
+
+const homeObserver = new IntersectionObserver(function(entries, homeObserver) {
+  entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        navHomeLink.classList.add("makeBold")
+        navHomeLink.classList.remove("makeBold")
+        dropDown.classList.remove("dropDownScroll")
+        dropDownUl.forEach(item => {
+          item.classList.remove("dropDownUlAdd")
+        })
+        secondContactButton.classList.remove("dropDownButtonAdd")
+        
+        
+      } else {
+        navHomeLink.classList.remove("makeBold")
+        dropDown.classList.add("dropDownScroll")
+        dropDownUl.forEach(item => {
+          item.classList.add("dropDownUlAdd")
+        })
+        secondContactButton.classList.add("dropDownButtonAdd")
+        
+      }
+      
+  })
+}, options)
+
+homeObserver.observe(heroSection)
 
 
 
