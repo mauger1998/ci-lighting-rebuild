42c6ce9b30c4d9adf9fee0d7c24502f6ae61541a
Jordan @ Island Web Design <maugernotminor@gmail.com>
2024-08-15 09:13:10 +0100

    Add new timing

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const contactButton = document.querySelector(".contact-button")
-const contactSection = document.querySelector(".contact")
-
-
+const contactButton = document.querySelector('.contact-button')
+const contactSection = document.querySelector('.contact')
 
 const swiper = new Swiper('.swiper', {
     // Optional parameters
@@ -9,131 +7,107 @@ const swiper = new Swiper('.swiper', {
     loop: true,
     autoplay: true,
     grabCursor: true,
-    effect: "creative",
-    speed:1000,
+    effect: 'creative',
+    speed: 1000,
     creativeEffect: {
         prev: {
-          shadow: true,
-          translate: [0, 0, -400],
+            shadow: true,
+            translate: [0, 0, -400],
         },
         next: {
-          translate: ["100%", 0, 0],
+            translate: ['100%', 0, 0],
         },
-      },
-  
+    },
+
     // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
+        el: '.swiper-pagination',
     },
-  
+
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
-  
-    
-  });
+})
 
 /**********************/
-contactButton.addEventListener("click", (e) => {
+contactButton.addEventListener('click', (e) => {
     contactSection.scrollIntoView()
 })
 /***********/
 
 let today = new Date()
-    let open = "Open Now"
-    let closed = "Closed Now"
-    let display = document.querySelector('.open-closed');
-    console.log(display)
+let open = 'Open Now'
+let closed = 'Closed Now'
+let display = document.querySelector('.open-closed')
+console.log(display)
 
-  
+let day = today.getDay()
+let hours = today.getHours()
+let minutes = today.getMinutes()
 
-
-    
-
-    
-
-if (today.getDay() == 6) {
-  if (today.getHours() >= 9 && today.getHours() <= 12 && today.getDay() != 0) {
-    display.textContent = open
-  } 
-} else {
-  if (today.getHours() >= 8 && today.getHours() <= 17 && today.getDay() !== 0) {
-    display.textContent = open
-} else  {
+if (day === 0 || day === 6) {
+    // Check if it's Saturday or Sunday
     display.textContent = closed
+} else {
+    if ((hours > 7 || (hours === 7 && minutes >= 30)) && hours < 17) {
+        display.textContent = open
+    } else {
+        display.textContent = closed
+    }
 }
-}
-
 
 /****************************/
-const heroSeeMoreButton = document.querySelector(".heroSeeMore")
-const whyUsSection = document.querySelector(".second-section")
-const cardButtons = document.querySelectorAll(".seemore")
-const productSection = document.querySelector(".products")
-const heroSection = document.querySelector(".hero-section")
+const heroSeeMoreButton = document.querySelector('.heroSeeMore')
+const whyUsSection = document.querySelector('.second-section')
+const cardButtons = document.querySelectorAll('.seemore')
+const productSection = document.querySelector('.products')
+const heroSection = document.querySelector('.hero-section')
 
-
-
-
-cardButtons.forEach(button => {
-  button.addEventListener("click", (e) => {
-    productSection.scrollIntoView()
-  })
+cardButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        productSection.scrollIntoView()
+    })
 })
 /*********************************/
 
+const navHomeLink = document.querySelector('.navHome a')
 
+const options = {}
 
-
-
-
-
-
-const navHomeLink = document.querySelector(".navHome a")
-
-const options = { }
-
-const homeObserver = new IntersectionObserver(function(entries, homeObserver) {
-  entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        navHomeLink.classList.add("makeBold")
-        navHomeLink.classList.remove("makeBold")
-        dropDown.classList.remove("dropDownScroll")
-        dropDownUl.forEach(item => {
-          item.classList.remove("dropDownUlAdd")
-        })
-        secondContactButton.classList.remove("dropDownButtonAdd")
-        
-        
-      } else {
-        navHomeLink.classList.remove("makeBold")
-        dropDown.classList.add("dropDownScroll")
-        dropDownUl.forEach(item => {
-          item.classList.add("dropDownUlAdd")
-        })
-        secondContactButton.classList.add("dropDownButtonAdd")
-        
-      }
-      
-  })
+const homeObserver = new IntersectionObserver(function (entries, homeObserver) {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            navHomeLink.classList.add('makeBold')
+            navHomeLink.classList.remove('makeBold')
+            dropDown.classList.remove('dropDownScroll')
+            dropDownUl.forEach((item) => {
+                item.classList.remove('dropDownUlAdd')
+            })
+            secondContactButton.classList.remove('dropDownButtonAdd')
+        } else {
+            navHomeLink.classList.remove('makeBold')
+            dropDown.classList.add('dropDownScroll')
+            dropDownUl.forEach((item) => {
+                item.classList.add('dropDownUlAdd')
+            })
+            secondContactButton.classList.add('dropDownButtonAdd')
+        }
+    })
 }, options)
 
 homeObserver.observe(heroSection)
 
+const burger = document.querySelector('.burger')
+const dropDown = document.querySelector('.dropDown')
+const secondContactButton = document.querySelector('.dropDownContact')
+const dropDownUl = document.querySelectorAll('.dropDown ul li a')
 
-const burger = document.querySelector(".burger")
-const dropDown = document.querySelector(".dropDown")
-const secondContactButton = document.querySelector(".dropDownContact")
-const dropDownUl = document.querySelectorAll(".dropDown ul li a")
-
-
-burger.addEventListener("click", (e) => {
-  dropDown.classList.toggle("dropDownAdd")
+burger.addEventListener('click', (e) => {
+    dropDown.classList.toggle('dropDownAdd')
 })
 
-secondContactButton.addEventListener("click", (e) => {
-  contactSection.scrollIntoView()
+secondContactButton.addEventListener('click', (e) => {
+    contactSection.scrollIntoView()
 })
-
